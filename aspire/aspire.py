d8ba598495150b1a3ad9647c2bfc390b9d103107
Arjuna S. Erat <sri-arjuna@users.noreply.github.com>
2023-07-02 18:45:27 +0200

    Init: ASPIRE

---
+++ aspire/aspire.py
@@ -0,0 +1,26 @@
+"""
+	Created on:		2023.July.02
+	Created by:		Simon Arjuna Erat
+	License:		MIT
+	URL:			http://www.github.com/sri-arjuna/ASPIRE
+"""
+
+from aspire_print_utils import PrintUtils
+
+class Aspire:
+	def __init__(self):
+		PrintUtils.print_text("Hello, World!")
+		PrintUtils.print_border()
+
+	@staticmethod
+	def header(str1, str2, str3):
+		# Implementation for header method
+
+	@staticmethod
+	def title(text):
+		# Implementation for title method
+
+	@staticmethod
+	def printe(v_left, v_center, v_right):
+		# Implementation for printe method
+
---
+++ aspire/aspire_print_utils.py
@@ -0,0 +1,22 @@
+"""
+	Created on:		2023.July.02
+	Created by:		Simon Arjuna Erat
+	License:		MIT
+	URL:			https://www.github.com/sri-arjuna/ASPIRE
+"""
+
+from aspire_dc_colors import ColorsBackground as color_bg
+from aspire_dc_colors import ColorsForeground as color_fg
+from aspire_dc_colors import TextCodes as tc
+
+class PrintUtils:
+	@staticmethod
+	def print_text(text):
+		# Implementation for print_text function
+		color_bg.red
+		tc.bold
+
+	@staticmethod
+	def print_border():
+		# Implementation for print_border function
+		color_fg.blue
