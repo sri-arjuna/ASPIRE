1e8d4deaab5b4d7058fe784e4615f1e4dd435190
Arjuna S. Erat <sri-arjuna@users.noreply.github.com>
2023-07-12 12:17:34 +0200

    Update Data_Interactions.py

--- examples/Data_Interactions.py
+++ examples/Data_Interactions.py
@@ -10,14 +10,35 @@ sys.path.append(aspire_dir)
 # Usualy, this would be only:
 #	from aspire import Aspire as tui
 from aspire.aspire import Aspire as tui
+from aspire.aspire_data_status import StatusEnum
+from aspire.aspire_data_status import dict_status
 
 
 tui.header("Simple Data Presentation")
 
 tui.title("Progress Bar")
-tui.progress("Shows a bar: 0/1", 0.1, 1)
+tui.progress("Shows a bar: 1/10", 0.1, 1)
 tui.progress("Shows a bar: 1/3", 1, 3)
 tui.progress("Shows a bar: 1/2", 1, 2)
 
 tui.title("User Interactions")
 yesno = tui.yesno("Are you happy?")
+if yesno:
+    tui.status(StatusEnum.Good, "Yes you are")
+else:
+    tui.status(StatusEnum.Bad,  "No, you are not happy.")
+tui.print()
+
+tui.title("Does also work with base bool / and + INT ")
+tui.status(yesno, "Bool result")
+tui.status(10 + yesno, "Int result: On/Off = 10+Bool")
+
+tui.print()
+tui.title("Call by dict")
+for key in dict_status:
+    tui.print(dict_status[key], eval(dict_status[key]).value, key)
+
+tui.print()
+tui.title("Call by Enum")
+for es in StatusEnum:
+    tui.status(es, f"Status: {es}")
