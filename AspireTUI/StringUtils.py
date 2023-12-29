926a1124386ab6c7588eac7631a22f9b79d64ae2
Arjuna S. Erat <erat.simon@gmail.com>
2023-12-29 19:24:53 +0100

    time & html preparations

--- AspireTUI/StringUtils.py
+++ AspireTUI/StringUtils.py
@@ -40,19 +40,68 @@ from dataclasses import dataclass as _dataclass
 #################################################################################################################
 #####                                           String Utils (stew)                                         #####
 #################################################################################################################
-def date():
+def date(bNames=False):
+	"""
+	Returns date like:
+	2024.12.30
+	"""
 	current_date = _datetime.now()
 	formatted_date = current_date.strftime("%Y.%m.%d")
+	if bNames:
+		formatted_date += f" {current_date.strftime('%A, %B')}"
 	return formatted_date
 
-def time():
+def time(bLong=False):
+	"""
+	Returns time like:
+	15:30
+	"""
 	current_time = _datetime.now()
 	formatted_time = current_time.strftime("%H:%M:%S")
+	if bLong:
+		formatted_time += f":{current_time.strftime('%S')}"
 	return formatted_time
 
-def now(clean=False, sep="/"):
+def now(clean=False, sep="/", bLong=False, bNames=False):
+	"""
+	Returns date and time like: \n
+	2024.12.30 / 15:45
+	"""
 	if clean:
-		return f"{date()} {time()}"
+		return f"{date(bNames)} {time(bLong)}"
 	else:
-		return f"{date()} {sep} {time()}"
+		return f"{date(bNames)} {sep} {time(bLong)}"
 
+def close_html_tags(html_string: str, exclude="") -> str:
+	"""
+	Close open HTML tags in the provided string.
+	Exclude closing "<div>" tags.
+	"""
+	# Match open tags
+	open_tags = _re.findall(r'<([^/][^>]*)>', html_string)
+
+	# Check for open tags from the end of the string
+	for tag in reversed(open_tags):
+		if exclude:
+			if not tag.startswith(exclude):
+				# Close the tag
+				html_string += f'</{tag}>'
+		else:
+			html_string += f'</{tag}>'
+	# No treatment required 
+	return html_string
+
+def esc2hex(strESC:str) -> str:
+	"""
+	Transforms internal color codes to web-hex-colors. \n
+	Returns an empty string if it didnt find something to close. \n
+	Example: colorA = esc2hex(cat.fg.white)
+	"""
+	from AspireTUI.ColorAndText import cat as _cat
+	if strESC == _cat.reset:
+		# Handle special case for cat.reset
+		return close_html_tags(strESC)
+	elif _re.match(r'<color=[^>]+>', strESC):
+			# Fallback: Close color tags
+			return _re.sub(r'<color=[^>]+>', '</color>', strESC)
+	return ""
