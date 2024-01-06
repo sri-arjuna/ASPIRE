4d1e8ce6f581ec6c5b332b4b3b73bd28fc1e32d3
Arjuna S. Erat <erat.simon@gmail.com>
2024-01-06 17:46:48 +0100

    Update MESSAGE.py
    Some messages for Class_LOG

--- AspireTUI/MESSAGE.py
+++ AspireTUI/MESSAGE.py
@@ -30,6 +30,10 @@ translation.install()
 ################################################################################################################
 word_error 			= _("Error")
 word_fatal			= _("Fatal")
+word_name			= _("Name")
+word_working		= _("Working")
+
+
 
 ################################################################################################################
 #####                                            Internal Values                                           #####
@@ -44,8 +48,8 @@ args_missing 		= _("Missing arguments!")
 ################################################################################################################
 #####                                            Files and Folders                                         #####
 ################################################################################################################
-file_not_found 		= _("File not found.")
-file_exists 		= _("File exists already.")
+file_not_found 		= _("File/Directory not found.")
+file_exists 		= _("File/Directory exists.")
 dir_not_found 		= _("Directory not found.")
 
 ################################################################################################################
@@ -68,5 +72,21 @@ theme_color_invalid	= _("Cant read color code, please use plain text, not consol
 tui_press			= _("Press any key to continue.")
 tui_progress_bar	= _("Wrong style for progress, only 'bar' and 'num' are supported.")
 tui_style_invalid	= _("Invalid style argument. Expected 'print', 'header', or 'title'. // passed:")
+################################################################################################################
+#####                                            Class Log                                                 #####
+################################################################################################################
+cl_log_severity0	= _("DEBUG")
+cl_log_severity1	= _("INFO")
+cl_log_severity2	= _("WARNING")
+cl_log_severity3	= _("ERROR")
+cl_log_severity4	= _("CRITICAL")
+cl_log_severity5	= _("FATAL")
+cl_log_fatal_no_file 			= _("No file provided, can not create class!")
+cl_log_warn_file_without_path	= _("Logfile does not contain any path information.\nThis could lead to unexpected behaviour")
+cl_log_err_must_bool		= _("Provided argument must be: bool")
+cl_log_err_must_int			= _("Provided argument must be: int")
+cl_log_err_must_str			= _("Provided argument must be: str")
+
+
 
 #tui_press			= _("")
