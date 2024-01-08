ecf7cf3b48d2145858700bb97b223ff28a052c68
Arjuna S. Erat <erat.simon@gmail.com>
2024-01-08 11:49:39 +0100

    Prepare local runtime test
    why not working anymore ?

--- examples/local_runtime.py
+++ examples/local_runtime.py
@@ -4,9 +4,10 @@ from os.path import abspath, dirname, join
 aspire_dir = abspath(join(dirname(__file__), '..'))
 sys.path.append(aspire_dir)
 
-from AspireTUI import tui
-from AspireTUI.ColorAndText import cat
-import AspireTUI._theme as Theme
-from AspireTUI import StringUtils as stew
+from ..aspire_dir.AspireTUI import tui
+from ..aspire_dir.AspireTUI.ColorAndText import cat
+import ..aspire_dir.AspireTUI._theme as Theme
+from ..aspire_dir.AspireTUI import StringUtils as stew
 
 print(stew.sec2time(75))
+tui.wait(5)
