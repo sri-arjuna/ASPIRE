832d3891acf210cfcfcceaea8b56904b8f3b4a05
Arjuna S. Erat <erat.simon@gmail.com>
2024-01-08 11:48:59 +0100

    Created new FileUtils
    Moved old FileUtils to/as classes/log

---
+++ AspireTUI/FileUtils.py
@@ -0,0 +1,25 @@
+"""
+	Description:
+					Public functions to easy several tasks with Files
+	Provides:
+					from AspireTUI import ConfigUtills as cu
+					ret_bool, ret_str = cu.reg_value_get(file,key,var)
+					
+	========================================================
+	Created on:		2024 Jan. 01
+	Created by:		Simon Arjuna Erat
+	License:		MIT
+	URL:			https://www.github.com/sri-arjuna/ASPIRE
+
+	Based on my TUI & SWARM for the BASH shell © 2011
+"""
+#
+#	Essential imports
+#
+#import os as _os
+#import sys as _sys
+#import re as _re
+#import string as _string
+from . import MESSAGE as _MSG
+from . import tui as _tui
+from typing import Union as _Union
