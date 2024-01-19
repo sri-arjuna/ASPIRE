59746bde5b42877e322a9a84cd962a9a0e10a6a2
Arjuna S. Erat <erat.simon@gmail.com>
2024-01-19 17:34:01 +0100

    added: isVerOS(minimal=None, bDual=False)
    	Checks if the OS version is at least "minimal". \n
    	If nothing is provided, Win 10 or Kernel 6.x are 'expected' as current system. \n
    	When criteria is met, it returns with True. \n
    
    	Use bDual=True if you want to return a bool and a 'uname' with OS information. \n
    	For Linux, version must be a one digit float like: 6.1, 5.9, 5.5

--- AspireTUI/os.py
+++ AspireTUI/os.py
@@ -22,7 +22,8 @@ import os as _os
 #import re as _re
 #import string as _string
 #from pathlib import Path as _Path
-from . import IS_WINDOWS as _IS_WINDOWS
+import platform as _platform
+from . import IS_WINDOWS
 #################################################################################################################
 #####                                           Get Directories                                             #####
 #################################################################################################################
@@ -51,7 +52,7 @@ def get_dir_OS(bVerbose=True, bMinimal=False):
 		task_list = []
 	# Start the job
 	for task in task_list:
-		if _IS_WINDOWS:
+		if IS_WINDOWS:
 			pass
 		else:
 			# Its a *nix based system
@@ -63,9 +64,49 @@ def isGUI():
 	"""
 	Check if the script is running in a GUI environment.
 	"""
-	if _IS_WINDOWS:
+	if IS_WINDOWS:
 		# Check if running in Windows console
 		return _os.getenv("TERM_PROGRAM") is not None
 	else:
 		# Check if running in a non-Windows environment
 		return _os.getenv("TERM") is None
+
+def isVerOS(minimal=None, bDual=False):
+	"""
+	Checks if the OS version is at least "minimal". \n
+	If nothing is provided, Win 10 or Kernel 6.x are 'expected' as current system. \n
+	When criteria is met, it returns with True. \n
+
+	Use bDual=True if you want to return a bool and a 'uname' with OS information. \n
+	For Linux, version must be a one digit float like: 6.1, 5.9, 5.5
+	"""
+	# Init vars
+	doLinux = True
+	if minimal is None:
+		if IS_WINDOWS:
+			minimal = 10
+			doLinux = False
+		else:
+			minimal = 6.1
+	elif isinstance(minimal, int):
+		doLinux = False
+	# Do the check
+		if doLinux:
+			print("# TODO: Linux needs to be verified if kernel detection works") # TODO
+			if int(_platform.release().split('.')[0]) >= str(minimal).split('.')[0]:
+				if int(_platform.release().split('.')[1]) >= str(minimal).split('.')[1]:
+					ret = True
+				else:
+					ret = False
+			else:
+				ret = False
+		else:
+			if int(_platform.release().split('.')[0]) >= minimal:
+				ret = True
+			else:
+				ret = False
+	# Return to user
+	if bDual:
+		return ret, _os.uname()
+	else:
+		return ret
