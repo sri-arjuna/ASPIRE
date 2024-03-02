915e89afc9e85763e075571aeae4d80f610d911a
Arjuna S. Erat <erat.simon@gmail.com>
2024-03-02 22:13:30 +0100

    Updated: Todo

--- tools/get_todo.py
+++ tools/get_todo.py
@@ -1,11 +1,26 @@
-import os
+import os, sys
+file_output = "TODO-Found.md"
 
-def find_todos(directory):
+import glob
+search_list =  ["OS.py", "Demo.py"] # ["File_1.py", "File_2.py", "File_3.py"]
+file_list = glob.glob(f'**/*.py', recursive=True)
+
+for item in file_list:
+	for search in search_list:
+		if search in item:
+			print("Found: ", item)
+sys.exit()
+
+
+import json
+
+def find_todos(directory: str):
 	"""
 	Recursively search for "TODO: " in all files within the given directory.
 	Create a markdown file ("TODO-Found.md") with the formatted output.
 	"""
 	todo_messages = {}
+	prefix = "./" + directory.replace("../", "") + " -/- "
 
 	def process_file(file_path):
 		nonlocal todo_messages
@@ -20,14 +35,14 @@ def find_todos(directory):
 
 	for root, dirs, files in os.walk(directory):
 		for file_name in files:
-			if file_name.endswith(".py"):  # Change the extension as needed
+			if not file_name.startswith("_") and file_name.endswith(".py"):
 				file_path = os.path.join(root, file_name)
 				process_file(file_path)
 
 	# Write TODO messages to a markdown file
-	with open("TODO-Found.md", 'w', encoding='utf-8') as output_file:
+	with open(file_output, 'w', encoding='utf-8') as output_file:
 		for file_path, messages in todo_messages.items():
-			output_file.write(f"\n# {file_path}\n")
+			output_file.write(f"\n# {prefix}{file_name}\n")
 			for message in messages:
 				output_file.write(message + '\n')
 		print("TODO messages written to TODO-Found.md")
