684515e2aaaf91a09b64feef9d4d1a561ba2f6f5
Arjuna S. Erat <erat.simon@gmail.com>
2024-06-17 23:46:39 +0200

    Update __init__.py

--- aspiretui/__init__.py
+++ aspiretui/__init__.py
@@ -23,23 +23,19 @@ from ._MESSAGES import current as _MSG 			# This is not used here, but eases the
 ################################################################################################################
 #
 #	Simplify OS access
+#	Get OS specific FileDescriptor (if possible, seperate output for the TUI)
 #
 if _os.name == 'nt':
 	# Basicly, detect if OS is Windows based
 	IS_WINDOWS=True
-else:
-	# Or *nix based
-	IS_WINDOWS=False
-#
-#	Get OS specific FileDescriptor (if possible, seperate output for the TUI)
-#
-if IS_WINDOWS:
 	# Windows does need the terminal to be... "initialized"... by this for the colors to work.
 	# No, subprocess.popen("", shell=True) does not work.
 	_os.system("")
 	# Windows does not support custom FileDescriptors
 	FD_BORDER=_sys.stderr
 else:
+	# Or *nix based
+	IS_WINDOWS=False
 	# *nix based Systems on the other hand, do support FIFO FD's
 	FD_BORDER=_os.fdopen(_os.dup(_sys.stderr.fileno()), 'w')
 ################################################################################################################
