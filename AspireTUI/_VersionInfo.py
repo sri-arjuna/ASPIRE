e2c642b62c3e62e13d832c441a37b2c1370e99e3
Arjuna S. Erat <erat.simon@gmail.com>
2024-09-11 00:01:22 +0200

    Updated version to 0.4.4
    Fixed progress bar
    Custom theme handling

--- AspireTUI/_VersionInfo.py
+++ AspireTUI/_VersionInfo.py
@@ -3,9 +3,9 @@
 """
 from . import Strings as _stew
 Created = "2023.11.09"
-Changed = "2024.09.04"
+Changed = "2024.09.10"
 Major = 0
 Minor = 4
-Build = 3
+Build = 4
 Version = f"{Major}.{Minor}.{Build}"
 FileGenComment = f"Created by AspireTUI ({Version} from {Changed}), https://github.com/sri-arjuna/ASPIRE" #on {_stew.now()}"
